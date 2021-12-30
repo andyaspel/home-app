8dc57cb055e3561e14b21ebbbfbb0c82723e51f5
andyaspel <aspelandy@outlook.com>
2021-12-30 09:12:46 +0100

    minor changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,42 @@
 <script>
-	import NavBar from "./library/navbarComponents/NavBar.svelte";
-	import SearchBar from "./library/formComponents/SearchBar.svelte";
-	 let name = "fuck!! it works!";
+  import NavBar from "./library/navbarComponents/NavBar.svelte";
+  import SearchBar from "./library/formComponents/SearchBar.svelte";
+  let name = "fuck!! it works!";
+  // let searchText = "searchText";
+  let  searchText ;
+  let test =[];
+  test.push(searchText)
+  
+  $: console.log(test);
+ 
 </script>
-<NavBar />
 
+<NavBar />
 <main>
-	<h1>Bollocks!!</h1>
-	<p>{name}</p>
-<SearchBar />
+  <h1>Bollocks!!</h1>
+  <p>{name}</p>
+  <SearchBar {searchText} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-		letter-spacing: 1.2rem;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+    letter-spacing: 1.2rem;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
--- src/library/formComponents/SearchBar.svelte
+++ src/library/formComponents/SearchBar.svelte
@@ -1,38 +1,41 @@
 <script>
-
-
+  export let searchText;
 </script>
 
+<form action="">
+  <label for="search-bar" aria-label="search-bar" />
+  <input
+    type="search"
+    name="search-bar"
+    id="search-bar"
+    placeholder="Search"
+    bind:value={searchText}
+  />
+</form>
+
 <style>
-form {
+  form {
     display: flex;
     justify-content: center;
     align-items: center;
     margin: 2rem auto;
-    padding:  0.6em 0;
+    padding: 0.6em 0;
     width: 45rem;
-    border: 0,1em solid hsla(270, 50%, 40%, 0.808);
+    border: 0, 1em solid hsla(270, 50%, 40%, 0.808);
     background-color: hsl(270, 14%, 78%);
     border-radius: 0.5em;
     box-shadow: 0.15rem 0.2rem 0.1rem 0.1rem rgba(255, 0, 245, 0.151);
-}
-form input {
+  }
+  form input {
     width: 92%;
     font-size: 1.3rem;
     font-weight: 500;
     color: hsl(243, 95%, 8%);
-    padding: 0.2em 0.4em;
+    padding: 0.2em 0.3em;
     margin: 0;
     border: none;
     border-radius: 0.25em;
 
-
     /* align-self: center; */
-}
-
+  }
 </style>
-
-<form action="">
-    <label for="search-bar" aria-label="search-bar"></label>
-    <input type="search" name="search-bar" id="search-bar" placeholder="Search">
-</form>
